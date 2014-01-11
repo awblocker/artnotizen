c531c52eda3ec932bf5d34fbd9d923c2058aad10
Alexander W Blocker <ablocker@gmail.com>
2014-01-11 12:22:20 -0800

    Switch to setuptools. Add dependencies.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 artnotizen
 ==========
 
-Markdown-based note organization
+Markdown-based note organization.
+
+Artnotizen uses [pandoc](http://johnmacfarlane.net/pandoc/) to compile Markdown to HTML. As this cannot be expressed as a Python dependency, it must be installed manually. On Debian-based Linux systems, you can use:
+```bash
+  sudo apt-get install pandoc
+```
--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 """Setup for artnotizen
 """
+import ez_setup
+ez_setup.use_setuptools()
 import os
-from distutils.core import setup
+from setuptools import setup
 
 setup(name="artnotizen",
       version="0.1",
-      url="http://www.awblocker.com",
+      url="https://github.com/awblocker/artnotizen/",
       description="Markdown-based note organization",
       author="Alexander W Blocker",
       author_email="ablocker@gmail.com",
+      license="Apache v2.0",
+      install_requires=["jinja2>=2.7"],
       # Keeping all Python code for package in lib directory
       package_dir={"artnotizen": "src"},
       packages=["artnotizen"],
